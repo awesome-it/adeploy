5bceae84209ed8fec4ea224d28d64978a4c21674
Daniel Morlock <daniel.morlock@awesome-it.de>
2020-12-17 10:40:23 +0100

    Check for recent GoPass version and raise a correct error if a secret was not found in Gopass.

--- adeploy/common/gopass.py
+++ adeploy/common/gopass.py
@@ -1,18 +1,37 @@
 import os
 import subprocess
+import re
+
 from logging import Logger
 from pathlib import Path
 from typing import Union
+from packaging.version import parse as parse_version
 
 from adeploy.common import colors
 from adeploy.common.args import get_args
+from adeploy.common.errors import InputError
 from adeploy.common.logging import get_logger
 
 
-def gopass_get_repos():
-    repos = get_args().gopass_repo
-    if repos is None or len(repos) == 0:
-        repos = os.getenv('ADEPLOY_GOPASS_REPOS', '').split(',')
+def gopass_get_version():
+    result = subprocess.run(['gopass', 'version'], capture_output=True, text=True)
+    result.check_returncode()
+    version_match = re.match(r"gopass ([^ ]*)", result.stdout.strip())
+
+    if not version_match:
+        raise InputError('Could not determine gopass version')
+
+    return version_match.group(1)
+
+
+def gopass_get_repos() -> [str]:
+    repos = [""]
+
+    if os.getenv('ADEPLOY_GOPASS_REPOS', False):
+        repos += os.getenv('ADEPLOY_GOPASS_REPOS', '').split(',')
+    else:
+        repos += get_args().gopass_repo
+
     return repos
 
 
@@ -24,6 +43,12 @@ def gopass_get(path: Union[Path, str], log: Logger = None) -> str:
     if not path:
         log.debug('Empty path, skipping call to gopass')
         return ""
+
+    # Check GoPass version
+    gopass_required_version = '1.10.0'
+    gopass_version = gopass_get_version()
+    if parse_version(gopass_version) < parse_version(gopass_required_version):
+        raise InputError(f'Found gopass version {gopass_version} but version {gopass_required_version}+ is required.')
 
     result = gopass_try_repos(path, True, log)
     if not result:
@@ -37,6 +62,7 @@ def gopass_get(path: Union[Path, str], log: Logger = None) -> str:
 
 def gopass_try_repos(path: Union[Path, str], explicit_pass = True, log: Logger = None) -> subprocess.CompletedProcess:
 
+    result = None
     for repo in [Path(r) for r in gopass_get_repos()]:
 
         repo_path = repo.joinpath(path)
@@ -46,4 +72,6 @@ def gopass_try_repos(path: Union[Path, str], explicit_pass = True, log: Logger =
 
         # Stop on success
         if not result.returncode and len(result.stdout.strip()) > 0:
-            return result
+            break
+
+    return result
