519224f841782e3d57e232661348629b41e7e09d
Peter Holzapfel <peter.holzapfel@awesome-it.de>
2026-02-27 16:52:28 +0100

    fix init

--- src/adeploy/common/secrets/__init__.py
+++ src/adeploy/common/secrets/__init__.py
@@ -2,3 +2,5 @@ from .secret import Secret
 from .generic_secret import GenericSecret
 from .tls_secret import TlsSecret
 from .docker_registry_secret import DockerRegistrySecret
+
+__all__ = ["Secret", "GenericSecret", "TlsSecret", "DockerRegistrySecret"]
